38d96061cf2332d87917ca266118f7d0c84dea97
Ramesh151 <rkmahto151@gmail.com>
2024-03-07 00:32:07 +0530

    projrct comp..

--- src/app/(home)/components/Project.tsx
+++ src/app/(home)/components/Project.tsx
@@ -1,5 +1,10 @@
+"use client";
 import React from 'react'
 import { SiNextdotjs, SiReact, SiTailwindcss } from 'react-icons/si'
+import Tittle from './tittle'
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
+import { DirectionAwareHover } from "@/components/ui/direction-aware-hover"
 
 
 function Project() {
@@ -17,10 +22,43 @@ function Project() {
       link:"https://bcaclasses.online/",
       cover:"/photo1.png",
       background:"bg-green-500"
+    },
+    {
+      title:"My Protfloi",
+      tech:[SiNextdotjs,SiReact,SiTailwindcss],
+      link:"https://bcaclasses.online/",
+      cover:"/photo1.png",
+      background:"bg-orange-500"
     }
   ]
   return (
-    <div>
+    <div className=' py-10 p-5 sm:p-0'>
+      <Tittle text='Projects 🛠️' className=' flex flex-col items-center justify-center -rotate-6' />
+      <div className=' grid grid-cols-1 sm:grid-cols-2 pt-20 gap-5'>
+
+        {
+          projects.map((project,index)=>{
+            return <Link href={project.link} key={index}>
+
+            <div className={cn("p-5 rounded-md",project.background)}>
+            <DirectionAwareHover imageUrl={project.cover} className=' w-full space-y-5  cursor-pointer' >
+          <h1 className=' text-2xl font-bold'>{project.title}</h1>
+
+           <div className=' flex items-center gap-5 '>
+            {project.tech.map((Icon,index)=>{
+              return <Icon className=' w-8 h-8 ' key={index}/>
+            })
+      }
+           </div>
+
+            </DirectionAwareHover>
+            </div>
+
+            </Link>
+          })
+        }
+
+      </div>
       
     </div>
   )
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import Skill from "./components/Skill";
+import Project from "./components/Project";
 
 export default function page() {
   return (
@@ -20,6 +21,7 @@ export default function page() {
 
     <div className="max-w-7xl mx-auto p-5 mt-20">
       <Skill/>
+      <Project/>
     </div>
     </div>
     
